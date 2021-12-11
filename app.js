8789a3ceaa262aeb6edbde531d1f0d9af01d07a5
Daniel Șerbănescu <daniel@serbanescu.dk>
2021-12-11 14:14:59 +0100

    Refactoring and cleanup of js and html

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 		releases = document.querySelector('select[name=releases]'),
 		list = document.querySelector('textarea[name=list]'),
 		src = document.querySelector('input[name=src]'),
-                backports = document.querySelector('input[name=backports]'),
+		backports = document.querySelector('input[name=backports]'),
 		contrib = document.querySelector('input[name=contrib]'),
 		nonfree = document.querySelector('input[name=non-free]'),
 		security = document.querySelector('input[name=security]');
@@ -14,7 +14,7 @@
 
 	var getComponents = function() {
 
-                var components = ['main'];
+		var components = ['main'];
 
 		if(contrib.checked) components.push('contrib');
 		if(nonfree.checked) components.push('non-free');
@@ -32,37 +32,37 @@
 	};
 
 	var generate = function() {
-		var ftp = mirror.options[mirror.selectedIndex].value,
-			rel = releases.options[releases.selectedIndex].value;
+		var url = mirror.options[mirror.selectedIndex].value,
+			dist = releases.options[releases.selectedIndex].value;
 
 		var comps = getComponents();
 		var arch = getArch();
 
-		appendSource(['deb', arch, ftp, rel, comps]);
-		if(src.checked) appendSource(['deb-src', arch, ftp, rel, comps]);
+		appendSource(['deb', arch, url, dist, comps]);
+		if(src.checked) appendSource(['deb-src', arch, url, dist, comps]);
 
 
 		if(releases.options[releases.selectedIndex].hasAttribute('data-updates')) {
 			appendSource(['']);
-			appendSource(['deb', arch, ftp, rel + '-updates', comps]);
-			if(src.checked) appendSource(['deb-src', arch, ftp, rel + '-updates', comps]);
+			appendSource(['deb', arch, url, dist + '-updates', comps]);
+			if(src.checked) appendSource(['deb-src', arch, url, dist + '-updates', comps]);
 		}
 
 		if(security.checked) {
 			appendSource(['']);
-			appendSource(['deb', arch, 'http://security.debian.org/', rel + '/updates', comps]);
-			if(src.checked) appendSource(['deb-src', arch, 'http://security.debian.org/', rel + '/updates', comps]);
+			appendSource(['deb', arch, 'http://security.debian.org/', dist + '/updates', comps]);
+			if(src.checked) appendSource(['deb-src', arch, 'http://security.debian.org/', dist + '/updates', comps]);
 		}
 
 		// When the backports are checked, excepting testing and unstable we add new lines to the output (den and deb-src).
-                if(backports.checked && rel != 'sid' && rel != 'bookworm') {
-                        appendSource(['']);
+		if(backports.checked && dist != 'sid' && dist != 'bookworm') {
+			appendSource(['']);
 			appendSource(['# Backports brings newer software to stable releases.']);
 			appendSource(['# Backports are disabled by default and are installed differently.']);
 			appendSource(['# For instructions check https://backports.debian.org/Instructions/ .']);
-                        appendSource(['deb', arch, ftp, rel + '-backports', comps]);
-                        if(src.checked) appendSource(['deb-src', arch, ftp, rel + '-backports', comps]);
-                }
+			appendSource(['deb', arch, url, dist + '-backports', comps]);
+			if(src.checked) appendSource(['deb-src', arch, url, dist + '-backports', comps]);
+		}
 
 
 		list.value = sourceList.join("\n");
